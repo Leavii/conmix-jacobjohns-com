d7503d474d06584389c8bdc71dfc0ae02e442224
jacob_johns <iam@jacobjohns.com>
2024-01-20 21:31:50 -0600

    Revert to 1a212c0

--- remix.config.js
+++ remix.config.js
@@ -1,15 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  server: "./server.ts",
-  serverBuildPath: "functions/[[path]].js",
-  serverConditions: ["workerd", "worker", "browser"],
-  serverDependenciesToBundle: "all",
-  serverMainFields: ["browser", "module", "main"],
-  serverMinify: true,
-  serverModuleFormat: "esm",
-  serverPlatform: "neutral",
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
+  serverModuleFormat: "cjs",
 };
